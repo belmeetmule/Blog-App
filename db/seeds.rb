a2ed3e5a0e7f8e9bfc692c488fe1689f4830603f
mule <belmeetmule@gmail.com>
2023-01-20 16:17:56 +0300

    linter errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
+
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   users = User.create([{ name: "Star Wars", photo: "http", bio: "Fuck that shit", posts_counter: 3})
+
 #   Character.create(name: "Luke", movie: movies.first)
