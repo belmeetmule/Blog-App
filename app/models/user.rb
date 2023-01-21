f3ffb0b07840a11c28814dbbee78be11e66664f8
mule <belmeetmule@gmail.com>
2023-01-21 18:02:22 +0300

    add validation to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id, dependent: :destroy
   has_many :comments, foreign_key: :author_id, dependent: :destroy
   has_many :likes, foreign_key: :author_id, dependent: :destroy
+  #validation
+  validates :name, presence: true
 
   def recent_three
     posts.order(created_at: :desc).limit(3)
