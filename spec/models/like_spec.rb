74094fd9fded3f0edd378bc41775e78275f82bf0
mule <belmeetmule@gmail.com>
2023-01-21 19:39:36 +0300

    add spec for the like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -8,4 +8,14 @@ RSpec.describe Like, type: :model do
   let(:like) do
     Like.create!(author_id: user.id, post_id: post.id)
   end
+
+  it 'is not valid without an author_id' do
+    like.author_id = nil
+    expect(like).to_not be_valid
+  end
+
+  it 'is not valid without a post_id' do
+    like.post_id = nil
+    expect(like).to_not be_valid
+  end
 end
