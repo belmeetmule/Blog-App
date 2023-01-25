97593d1d4f00c7d54214f679364101b2619ae52f
mule <belmeetmule@gmail.com>
2023-01-25 16:58:13 +0300

    add request spec for users controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,17 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+  it 'should render index' do
+    get '/'
+    expect(response).to render_template(:index)
+  end
+
   describe 'GET /index' do
     it 'returns http success' do
       get '/users/index'
       expect(response).to have_http_status(:success)
     end
+
+    it 'should include correct placeholder' do
+      get '/'
+      expect(response.body).to include("The list of all users can be found at '/' or 'users/'")
+    end
   end
 
+ 
+
   describe 'GET /show' do
+    before :each do
+      get '/users/:id'
+    end
+
     it 'returns http success' do
-      get '/users/show'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'should return show' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'should include correct placeholder' do
+      expect(response.body).to include("An individual user whose id is in the url. You are at '/user/:id'")
     end
   end
 end
