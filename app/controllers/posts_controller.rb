f20cf174ade44bf2143c36af1f37976467a3696a
mule <belmeetmule@gmail.com>
2023-01-28 19:06:38 +0300

    add new and create actions into posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,26 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
   end
+
+  def new
+    @new_post = Post.new
+  end
+
+  def create
+    post = Post.new(author: current_user, **post_params)
+
+    if post.save
+      flash[:success] = 'Post saved successfully'
+      redirect_to user_posts_url
+    else
+      flash.now[:error] = 'Error: Post could not be saved'
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
