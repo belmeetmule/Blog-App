8692bbfa5f8d684c76e31edca7873f1350781bd8
mule <belmeetmule@gmail.com>
2023-01-28 20:26:28 +0300

    fix rubocop errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,24 @@
 class CommentsController < ApplicationController
-    def new
-        @comment = Comment.new
-        @post = Post.find(params[:post_id])
-      end
-    
-      def create
-        post = Post.find(params[:post_id])
-        comment = Comment.create(author: current_user, post:, **comment_params)
-    
-        if comment.save
-          flash[:success] = 'Comment was successfully saved !'
-        else
-          flash.now[:error] = 'Error: Comment could not be saved'
-        end
-        redirect_to user_post_url(current_user.id, post.id)
-      end
-    
-      private
-    
-      def comment_params
-        params.require(:comment).permit(:text)
-      end
+  def new
+    @comment = Comment.new
+    @post = Post.find(params[:post_id])
+  end
+
+  def create
+    post = Post.find(params[:post_id])
+    comment = Comment.create(author: current_user, post:, **comment_params)
+
+    if comment.save
+      flash[:success] = 'Comment was successfully saved !'
+    else
+      flash.now[:error] = 'Error: Comment could not be saved'
+    end
+    redirect_to user_post_url(current_user.id, post.id)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
