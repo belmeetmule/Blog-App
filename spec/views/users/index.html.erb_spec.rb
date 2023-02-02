e2d733cf5124bf7c0bd703803f72b7210370acc5
mule <belmeetmule@gmail.com>
2023-02-02 20:13:39 +0300

    add spec for user index and user show views

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe 'users/index.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+require 'rails_helper'
+RSpec.describe 'User index', type: :feature do
+  before :each do
+    @author = User.create(name: 'Mulugeta',
+                          photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                          bio: 'Mv Student')
+    visit users_path
+  end
+  it 'shows user name' do
+    expect(page).to have_content(@author.name)
+  end
+  it 'shows the user photo' do
+    image = page.all('img')
+    expect(image.size).to eq 1
+  end
+  it 'shows number of posts user has written' do
+    expect(page.body).to include('0')
+  end
+  it 'redirects to user show page when a user is clicked' do
+    click_link @author.name
+    expect(current_path).to match user_path(@author)
+  end
 end
