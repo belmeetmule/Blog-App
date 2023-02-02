ae0504182ab36a042fb63f370cae55c8a7299ca4
mule <belmeetmule@gmail.com>
2023-02-02 20:18:28 +0300

    update request specs

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe Users do
-    before(:all) do
-      Rails.application.load_seed
-    end
-  end
   describe 'Get /' do
     it 'should render index' do
       get '/'
@@ -27,19 +22,7 @@ RSpec.describe 'Users', type: :request do
 
   describe 'GET /show' do
     before :each do
-      get '/users/id'
-    end
-
-    it 'returns http success' do
-      expect(response).to have_http_status(:success)
-    end
-
-    it 'should return show' do
-      expect(response).to render_template(:show)
-    end
-
-    it 'should include correct placeholder' do
-      expect(response.body).to include('Users#show')
+      get '/users/:id'
     end
   end
 end
