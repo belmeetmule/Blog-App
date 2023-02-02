0974688c5cdfe6cc855c86ea65c0c8a5a23a2186
mule <belmeetmule@gmail.com>
2023-02-03 00:06:14 +0300

    set pay defaults

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,4 +1,4 @@
 Pagy::DEFAULT[:items] = 3  # items per page
 
 # When you are done setting your own default freeze it, so it will not get changed accidentally
-Pagy::DEFAULT.freeze
+#Pagy::DEFAULT.freeze
