55a5eac4136271957a5a468d04842ce326416032
mule <belmeetmule@gmail.com>
2023-02-07 21:45:21 +0300

    remove unused code as per the CR comments

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,39 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  # describe 'GET /index' do
-  #   before :each do
-  #     get '/users/:user_id/posts/'
-  #   end
 
-  #   it 'returns http success' do
-  #     expect(response).to have_http_status(:success)
-  #   end
-
-  #   it 'should render index' do
-  #     expect(response).to render_template(:index)
-  #   end
-
-  #   it 'should include correct placeholder' do
-  #     expect(response.body).to include('Posts#index')
-  #   end
-  # end
-
-  # describe 'GET /show' do
-  #   before :each do
-  #     get '/users/:user_id/posts/:id'
-  #   end
-
-  #   it 'returns http success' do
-  #     expect(response).to have_http_status(:success)
-  #   end
-
-  #   it 'should render show' do
-  #     expect(response).to render_template('posts/:show')
-  #   end
-
-  #   it 'should include correct placeholder' do
-  #     expect(response.body).to include('Posts#index')
-  #   end
-  # end
 end
