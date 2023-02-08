a955c572efece262b88bb1cc8bf4302bc742776a
mule <belmeetmule@gmail.com>
2023-02-08 15:49:01 +0300

    update migration and remove unnecessary migration files

--- db/migrate/20230123211428_add_counters_to_comments.rb
+++
@@ -1,6 +0,0 @@
-class AddCountersToComments < ActiveRecord::Migration[7.0]
-  def change
-    add_column :comments, :comments_counter, :integer, default: 0
-    add_column :comments, :likes_counter, :integer, default: 0
-  end
-end
