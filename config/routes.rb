9bcb96c24f292a58e0b17882790c870919ff0012
amon-cofie <ddd.amon123@gmail.com>
2023-02-10 11:10:42 +0000

    Update routes.rb file to create namese for API

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   devise_scope :user do
     root to: "devise/sessions#new"
   end
-  
+
+  namespace :api do
+    resources :users
+  end
+
   resources :users, only: [:index, :show] do 
     resources :posts, only: [:index, :show, :new, :create, :destroy] do 
       resources :comments, only: [:new, :create, :destroy]
