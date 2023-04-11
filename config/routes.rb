87519969bd06844244852d15925a000fdbb1084d
mule <belmeetmule@gmail.com>
2023-04-11 23:21:21 +0300

    Update routes to fix routing errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,8 @@
 Rails.application.routes.draw do
-  #devise root
-  devise_for :users
-  devise_scope :user do
-    root to: "devise/sessions#new"
-  end
-
-   # API routes
-   namespace :api do
-    namespace :v1 do
-      resources :users do 
-        resources :posts do 
-          resources :comments
-          resources :likes
-        end
-      end
-    end
-  end
-
+  root "users#index"
   resources :users, only: [:index, :show] do 
-    resources :posts, only: [:index, :show, :new, :create, :destroy] do 
-      resources :comments, only: [:new, :create, :destroy]
+    resources :posts, only: [:index, :show, :new, :create] do 
+      resources :comments, only: [:new, :create]
       resources :likes, only: [:create]
     end
   end
